59ce099533d4ff188b06ae1f4804072ecb0fb635
tamilselvan1221 <115525607+tamilselvan1221@users.noreply.github.com>
2025-03-10 18:30:34 +0530

    Update exercise1.java

--- exercise1.java
+++ exercise1.java
@@ -1,65 +1,37 @@
-import java.util.Scanner;
+import javax.swing.*;
 
-public class Confidentiality {
-
-    // Encrypt function
-    public static String encrypt(String input) {
-        StringBuilder encrypted = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            encrypted.append((char)(input.charAt(i) + 3));
+public class EncryptDecrypt {
+    
+    public static String encrypt(String text) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            encryptedText.append((char) (c + 3));
         }
-        return encrypted.toString();
+        return encryptedText.toString();
     }
-
-    // Decrypt function
-    public static String decrypt(String input) {
-        StringBuilder decrypted = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            decrypted.append((char)(input.charAt(i) - 3));
+    
+    public static String decrypt(String text) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            decryptedText.append((char) (c - 3));
         }
-        return decrypted.toString();
+        return decryptedText.toString();
     }
-
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-        char ch;
-
-        System.out.println("\t\tConfidentiality");
-        do {
-            System.out.println("\t\tChoose operation");
-            System.out.println("\t\t1. Encryption");
-            System.out.println("\t\t2. Decryption");
-            System.out.println("\t\t3. Exit");
-            System.out.print("\t\tChoice: ");
-            choice = scanner.nextInt();
-            scanner.nextLine();  // Consume the newline character
-
-            switch (choice) {
-                case 1:
-                    System.out.print("\t\tInput String: ");
-                    String encryptStr = scanner.nextLine();
-                    System.out.println("\t\tEncrypted String: " + encrypt(encryptStr));
-                    break;
-                case 2:
-                    System.out.print("\t\tInput String: ");
-                    String decryptStr = scanner.nextLine();
-                    System.out.println("\t\tDecrypted String: " + decrypt(decryptStr));
-                    break;
-                case 3:
-                    System.out.println("\t\tExiting...");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("\t\tInvalid choice.");
-                    break;
-            }
-
-            System.out.print("\n\t\tDo you want to continue (Y/N): ");
-            ch = scanner.nextLine().charAt(0);
-
-        } while (ch == 'y' || ch == 'Y');
-
-        scanner.close();
+        while (true) {
+            String[] options = {"Encrypt", "Decrypt", "Exit"};
+            int choice = JOptionPane.showOptionDialog(null, "Choose an operation", "Confidentiality",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+            
+            if (choice == 2 || choice == -1) break;
+            
+            String input = JOptionPane.showInputDialog("Enter String:");
+            if (input == null) break;
+            
+            String result = (choice == 0) ? encrypt(input) : decrypt(input);
+            
+            JOptionPane.showMessageDialog(null, "Result: " + result);
+        }
     }
 }
